6c14560bea4e4b6bc1309b059c69499fad24554c
unknown <rjaesguerra@gmail.com>
2015-09-08 09:46:00 -0700

    Added configuration file for Twitter security parameters

---
+++ clientConfig.js
@@ -0,0 +1,8 @@
+/**
+ * Created by rjaesguerra on 9/8/2015.
+ */
+
+process.env['CONSUMER_KEY'] = ""
+process.env['CONSUMER_SECRET'] = ""
+process.env['ACCESS_TOKEN_KEY'] = ""
+process.env['ACCESS_TOKEN_SECRET'] = ""
